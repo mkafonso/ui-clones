293a5765718f4c34a4f2be261cd8215be22f6c07
mkafonso <makaya2afonso@gmail.com>
2020-10-27 23:07:26 -0300

    animate columns

---
+++ pinterest-homepage/scripts/main.js
@@ -0,0 +1,30 @@
+const grids = document.querySelectorAll(".grid");
+const headings = document.querySelectorAll(".heading .wrapper .text");
+
+function enterScreen(index) {
+  const grid = grids[index];
+  const heading = headings[index];
+  const gridColumns = grid.querySelectorAll(".column");
+
+  grid.classList.add("active");
+
+  gridColumns.forEach((element) => {
+    element.classList.remove("animate-before");
+  });
+}
+
+function exitScreen() {}
+
+function setupAnimationCircle({
+  initialScreenIndex,
+  timePerScreen,
+  exitDelay,
+}) {
+  enterScreen(0);
+}
+
+setupAnimationCircle({
+  initialScreenIndex: 0,
+  timePerScreen: 2000,
+  exitDelay: 200 * 7,
+});
